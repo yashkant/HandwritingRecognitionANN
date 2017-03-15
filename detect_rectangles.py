97b4d65c412b693f26c1bf3ebc3e62344879de5f
Yash <ysh.kant@gmail.com>
2017-03-15 17:43:16 +0530

    Changes in getp function.

--- detect_rectangles.py
+++ detect_rectangles.py
@@ -1,8 +1,17 @@
+
+# coding: utf-8
+
+# In[184]:
+
 import cv2 
 import numpy as np 
 import math
 import scipy.misc
 import os
+from PIL import Image
+
+
+# In[185]:
 
 #This function extracts the opposite points of a rectangle.
 def getp(rect):
@@ -15,9 +24,16 @@ def getp(rect):
     return[[x[1],x[2]],[y[1],y[2]]]
 
 
+# In[186]:
+
+print getp([[[3,5]],[[4,10]],[[10,4]],[[12,13]]])
+
+
+# In[187]:
+
 #This function arranges the images in order from left to right and also gets the diagonal points 
 def arrange(rects):
-	ar = [[[1001]]]
+	ar = [[[1001000]]]
 	for r in rects:
 		tr = getp(r)
 		print tr
@@ -27,16 +43,25 @@ def arrange(rects):
 				break
 	return ar[:-1]
 
+
+# In[197]:
+
 def saveimages(crops):
 	for i in range(1,len(crops)+1) :
 		scipy.misc.imsave('outputs/images/outfile'+str(i)+'.jpg', crops[i-1])
+        
+
+
+# In[189]:
 
 def savedata(crops):
-	cwd = os.getcwd()
-	os.chdir( cwd + '/outputs/data')
-	for i in range(0,len(crops)):
-		crops[i].dump("data" + str(i+1) +".data")
-	os.chdir(cwd)
+    os.chdir( cwd + '/outputs/data')
+    for i in range(0,len(crops)):
+        crops[i].dump("data" + str(i+1) +".data")
+    os.chdir(cwd)
+
+
+# In[190]:
 
 #This function provides us with six nearly equal rectangles 
 def filter(rects):
@@ -54,33 +79,42 @@ def filter(rects):
 	for tp in tps:
 		if (len(tp) == 7 and tp[0] > 28*28):
 			return tp[1:]
-			
 
-im = cv2.imread('nnsample_box.png')
+
+# In[191]:
+
+print os.getcwd()
+os.chdir('/home/yash/Desktop/Neural Networks/Handwriting Recognition')
 #Saves the dimens of image, I wish to resize the image proportional to its original dimensions.
 #The scaling factor is such that width will be 1000 else lesser for low pixel image.
+im = cv2.imread('nnsample_box.png')
 height,width = im.shape[:2]
-rwidth = 1000
-sfactor = float(width)/rwidth
-rheight = int(math.ceil(float(height)/sfactor ))
-if(sfactor > 1):
-	im = cv2.resize(im, (rwidth,rheight))
+# rwidth = 1000
+# sfactor = float(width)/rwidth
+# rheight = int(math.ceil(float(height)/sfactor ))
+# if(sfactor > 1):
+# 	im = cv2.resize(im, (rwidth,rheight))
 
 imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 imgray = cv2.fastNlMeansDenoising(imgray,None,10,7,21)
 
 
+# In[192]:
 
 #Thresholding requires a grayscale image 2nd param : threshvalue and 3rd param : maxValue of a pixel
 ret,thresh = cv2.threshold(imgray,125,255,0)
 ret,thresh = cv2.threshold(imgray,125,255,0)
 thresh = (255-thresh)
+print os.getcwd()
 #Closing is dialation followed by erosion helps to fill out the gaps left out by creases in paper or disconnected components.
 #Size of kernel is area of sliding window, I think it should be proportional to the size of image/boxes we will be using.
 ki = int(math.ceil(float(width)/100))
 # kernel = np.ones((ki,ki), np.uint8)
 kernel = np.ones((4,4), np.uint8)
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+
+# In[193]:
 
 #Countours are curves joining all the continuous points having same colour or intensity.
 #http://opencvpython.blogspot.in/2012/06/hi-this-article-is-tutorial-which-try.html
@@ -95,9 +129,10 @@ im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPRO
 approx = []
 for i in range(0,len(contours)) :
 	epsilon = 0.025*cv2.arcLength (contours[i],True)
-	print epsilon
 	approx.append(cv2.approxPolyDP(contours[i],epsilon,True))
 
+
+# In[194]:
 
 #Separate the ones which are rectangles. :)
 rects = []
@@ -111,6 +146,9 @@ for i in range(0,len(approx)):
 if(len(rects) > 6):
 	rects = filter(rects)
 
+
+# In[195]:
+
 #Display detected rectangles not arranged on x axis till now.
 im = cv2.imread('nnsample_box.png')
 for i in range(0,len(rects)) :
@@ -120,46 +158,26 @@ for i in range(0,len(rects)) :
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
+
+# In[198]:
+
+
 #Arrange in ascending order of x and put in opposite points 
 frects = arrange(rects)
 
 #Cropping required from the inner sides of the edges.
 im = cv2.imread('nnsample_box.png')
+
 crops = []
 for r in frects:
-	crops.append( im[ r[1][0]:r[1][1], r[0][0]:r[0][1] ] )
-
+    crops.append( im[ r[1][0]:r[1][1], r[0][0]:r[0][1] ] )
+for crop in crops:
+    cv2.imshow('crops',crop)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
 saveimages(crops)
 
 
 
-# cv2.imshow('Resized',thresh)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 
-
-# for i in range(0,len(rects)) :
-# 	area = cv2.contourArea(rects[i])
-# 	cv2.drawContours(im,rects,i,(0,255,0),2)
-# 	cv2.imshow('filtered',im)
-# 	print "Area = " + str(area)+"    " +str(len(contours[i])) + "    " +str(i)
-# 	print "Area = " + str(area)+"    " +str(len(approx[i])) + "    " +str(i)
-# 	cv2.waitKey(0)
-# 	cv2.destroyAllWindows()
-
-
-# for i in range(0,len(rects)) :
-# 	area = cv2.contourArea(rects[i])
-# 	cv2.drawContours(im,rects,i,(((i%2)+1)*255,(i%2)*255,0),2)
-# 	cv2.imshow('before',im)
-# 	cv2.waitKey(0)
-# 	cv2.destroyAllWindows()
-
-# 	cv2.drawContours(imc,approx,i,(0,255,0),2)
-# 	cv2.imshow('after',imc)
-# 	print "Area = " + str(area)+"    " +str(len(contours[i])) + "    " +str(i)
-# 	print "Area = " + str(area)+"    " +str(len(approx[i])) + "    " +str(i)
-# 	# cv2.imshow('result', im)
-# 	# raw_input("Press Enter to terminate.")
-# 	cv2.waitKey(0)
-# 	cv2.destroyAllWindows()
--- opencv.py
+++ opencv.py
@@ -3,64 +3,46 @@ import numpy as np
 import Neural_Networks as nn 
 import Load_MNIST as lm 
 
+def predictImage(number):
+	#This will check results to each separated image.
+	img = cv2.imread('outputs/images/outfile'+str(number)+'.jpg')
+	rimg = cv2.resize(img, (28,28))
+	rimg = cv2.GaussianBlur(rimg,(5,5),0)
+	rimg = cv2.cvtColor( rimg, cv2.COLOR_RGB2GRAY )
+	# ret,rimg = cv2.threshold(rimg,127,255,cv2.THRESH_BINARY)
+	dip = 255 - rimg 
+	# dip = cv2.erode(dip,kernel,iterations = 1)
+	# dip = cv2.dip(img,kernel,iterations = 1)
+	#This converts the integer values to those of float32 as required in MNIST dataset 
+	dip = dip.astype(np.float32, copy=False)
+	dip = dip/255
+	dip = np.reshape(dip,(784,1))
+	#Change this manual or use a better for the bottom values
+	#filter according to the dataset.
+	for a in dip :
+		if(a[0] < 0.43):
+			a[0] = 0
+		if(a[0] > 0.5):
+			a[0] = 1
 
+	dip = np.reshape(dip,(28,28))
+	cv2.imshow('Dip', dip)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	dip = np.reshape(dip,(784,1))
+	#dip is ready to be tested using the network 
+	train_data , validate_date , test_data = lm.prepare_dataset()
+	net = nn.Network([784,30,20,10])
+	net.load()
+	print net.feedforward(dip)
+	print np.argmax(net.feedforward(dip))
 
+#50% Accuracy as of now has been acheived.
+for i in range (1,7):
+	predictImage(i)
 
-img = cv2.imread('3a.jpg')
-rimg = cv2.resize(img, (100,100))
-rimg = cv2.GaussianBlur(rimg,(5,5),0)
-
-cv2.imshow('Dip', rimg)
-cv2.imshow('Dip1', edges)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-rimg = cv2.cvtColor( rimg, cv2.COLOR_RGB2GRAY )
-# rimg = cv2.GaussianBlur(rimg,(100,100),0)
-ret,rimg = cv2.threshold(rimg,127,255,cv2.THRESH_BINARY)
-dip = 255 - rimg 
-#print rimg 
-# dip = cv2.erode(dip,kernel,iterations = 1)
-# dip = cv2.dip(img,kernel,iterations = 1)
-
-# print type(binarize)
-
-#This converts the integer values to those of float32 as required in MNIST dataset 
-dip = dip.astype(np.float32, copy=False)
-dip = dip/255
-
-
-dip = np.reshape(dip,(10000,1))
-
-for a in dip :
-	if(a[0] < 0.43):
-		a[0] = 0
-	if(a[0] > 0.5):
-		a[0] = 1
-
-dip = np.reshape(dip,(100,100))
-
-
-
-
-
-dip = np.reshape(dip,(784,1))
-
-#print dip
-#dip is finally ready to be tested using the network 
-train_data , validate_date , test_data = lm.prepare_dataset()
-#print test_data[0][0]
-net = nn.Network([784,30,20,10])
-net.load()
-print net.feedforward(dip)
-print np.argmax(net.feedforward(dip))
 # net.stochastic_gradient_descent(train_data,3,10,3.0,test_data)
 # net.save()
-
-
-
 # print '{0} -> {1}'.format(np.argmax(net.feedforward(dip)), 2)
-
 #Images captured with devices would also require contrast adjustments since binarizing has not been a good alternative 
 
