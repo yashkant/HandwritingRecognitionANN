064adeefe74dbe223312058cf54b2540b33500f0
Nirmal <nirmalsarswat400@gmail.com>
2017-11-24 07:14:30 +0530

    a

---
+++ 2k178_codes/segmentations.py
@@ -0,0 +1,93 @@
+import PIL
+import matplotlib
+import cv2
+print cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# image = cv2.imread('colored_digit.jpg')
+image = cv2.imread('yellow-alphabate.jpg')
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('gray_image.png', gray_image)
+img = cv2.imread('gray_image.png')
+# cv2.imshow('color_image', image)
+# cv2.imshow('gray_image', gray_image)
+  # Waits forever for user to press any key
+# cv2.destroyAllWindows()  # Closes displayed windows
+blur = cv2.bilateralFilter(img,9,75,75)
+cv2.imwrite('bilateral_img.png',blur)
+bilateral_img = cv2.imread('bilateral_img.png',0)
+cv2.imshow('bilateral_img.png',bilateral_img)
+img = cv2.imread('bilateral_img.png',0)
+# img = cv2.medianBlur(img,5)
+# way to implement gaussian thresholding
+ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+for i in xrange(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
+# way to implement OTSU thresholding
+
+'''ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+# Otsu's thresholding
+ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# Otsu's thresholding after Gaussian filtering
+blur = cv2.GaussianBlur(img,(5,5),0)
+ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# plot all the images and their histograms
+images = [img, 0, th1,
+          img, 0, th2,
+          blur, 0, th3]
+titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
+          'Original Noisy Image','Histogram',"Otsu's Thresholding",
+          'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
+for i in xrange(3):
+    plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
+    plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+    plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+    plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+plt.show()
+cv2.waitKey(0)
+kernel = np.ones((5,5), np.uint8)
+img_dilation = cv2.dilate(th3, kernel, iterations=1)
+cv2.imshow('dilated',img_dilation)
+cv2.waitKey(0)
+
+#find contours
+im2,ctrs, hier = cv2.findContours(img_dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+#sort contours
+sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+
+for i, ctr in enumerate(sorted_ctrs):
+    # Get bounding box
+    x, y, w, h = cv2.boundingRect(ctr)
+
+    # Getting ROI
+    roi = image[y:y+h, x:x+w]
+
+    # show ROI
+    cv2.imshow('segment no:'+str(i),roi)
+    cv2.rectangle(image,(x,y),( x + w, y + h ),(90,0,255),2)
+    cv2.waitKey(0)
+
+'''
+cv2.imshow('marked areas',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# End of Code
